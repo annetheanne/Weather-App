162cfc39902a47619614ff10c1cf4fc264fd9e94
Anne Huynh <huynh.anne@gmail.com>
2022-08-12 14:53:40 -0700

    Update script.js

--- src/script.js
+++ src/script.js
@@ -64,7 +64,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "5e1b356a3b12f393ea0d4970a95367ca";
+  let apiKey = "125f64a9971676c48141e4f1453acef7";
   let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(displayForecast);
 }
